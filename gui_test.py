4ffe7ec92aed7af799d97035ea55fcd90f37fccb
Joylana <alanajrm@yahoo.com>
2026-02-25 11:51:02 -0600

    working display window

--- gui_test.py
+++ gui_test.py
@@ -3,17 +3,27 @@ from joblib import load
 import pandas as pd
 
 # Load the lr model back into a variable
-loaded_model = load('lrmodel.joblib') # load model
-preprocessor = load('preprocessor.joblib') # load pipeline
+loaded_model = load('/Users/alanajoymorrison/Desktop/Capstone/lrmodel.joblib') # load model
+preprocessor = load('/Users/alanajoymorrison/Desktop/Capstone/preprocessor.joblib') # load pipeline
 
-test = pd.read_csv('datasets/life_expectancy_test_data.csv')# load test data 
+test = pd.read_csv('/Users/alanajoymorrison/Desktop/Capstone/datasets/life_expectancy_test_data.csv')# load test data 
 test = test[10:11] # get one row of data to test the GUI with
 
 X = preprocessor.transform(test) # transform test data
 
 # predict
 prediction = loaded_model.predict(X,)
+print(prediction)
 
 
 #running a quick test of the GUI to make sure it is working.
-#import tkinter as tk
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Life Expectancy Prediction")
+label = tk.Label(root, text="Predicted Life Expectancy: " + str(prediction[0]), 
+                 font=("Arial", 16))
+label.pack(pady=20)
+
+root.mainloop()
